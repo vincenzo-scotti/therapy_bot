ae5fd4b548349b60979ccb7c0dfe9c3d21c337c6
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2023-02-05 18:16:28 +0100

    Fix keyboard issue

--- src/therapy_bot/telegram/utils.py
+++ src/therapy_bot/telegram/utils.py
@@ -3,5 +3,5 @@ from telegram import ReplyKeyboardMarkup
 
 IDLE, CHAT, EVAL = range(3)
 
-EVAL_KEYBOARD = [str(i + 1) for i in range(5)]
+EVAL_KEYBOARD = [[str(i + 1)] for i in range(5)]
 EVAL_MARKUP = ReplyKeyboardMarkup(EVAL_KEYBOARD, one_time_keyboard=True)
